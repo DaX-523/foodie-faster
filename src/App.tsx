86f0c58a68bd5849c02854c17f36f8d0e9e10128
DaX-523 <dakshdhama0502@gmail.com>
2023-08-23 02:17:57 +0530

    added dynamic rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { resData } from "../assets/data";
 
 const HeaderComponent = () => {
   return (
     <div className="nav-bar">
-      <img className="img" src="https://banner2.cleanpng.com/20180702/vrt/kisspng-online-food-ordering-food-delivery-grubhub-5b3a1b75a36132.9626073015305347736692.jpg" alt="logo" />
+      <img className="img" src="https://www.shutterstock.com/shutterstock/photos/788158552/display_1500/stock-vector-food-delivery-in-black-color-delivery-label-for-online-shopping-worldwide-shipping-vector-788158552.jpg" alt="logo" />
       <div className="nav-links">
 
       <ul>
@@ -17,12 +18,17 @@ const HeaderComponent = () => {
   )
 }
 
-const RestaurantCardComponent = () => {
+const RestaurantCardComponent = (props: any) => {
+  const {restaurantData} = props;
+  const restaurant = restaurantData.info;
+  const cuisines = restaurant.cuisine.map((elem: any) => elem.name)
   return (
     <div className="card-container">
-      <img className="card-img" src="https://b.zmtcdn.com/data/pictures/chains/6/19447676/abd8bcf7f3c172b6a7f9058b0e21067c.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*" alt="restaurant image" />
-      <h3>Makhan Singh Da Dhaba</h3>
-      <h4>Indian, Desi</h4>
+      <img className="card-img" src={restaurant.image.url} alt="restaurant image" />
+      <h3>{restaurant.name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{restaurant.rating.aggregate_rating}</h4>
+      <h4>{restaurantData.order.deliveryTime}</h4>
     </div>
   )
 }
@@ -34,12 +40,8 @@ const BodyComponent = () => {
       Search
     </div>
     <div className="container">
-      <RestaurantCardComponent />
-      <RestaurantCardComponent />
-      <RestaurantCardComponent />
-      <RestaurantCardComponent />
-      <RestaurantCardComponent />
-      <RestaurantCardComponent />
+      {resData.section.map((restaurant: any) => <RestaurantCardComponent key={restaurant.info.resId} restaurantData={restaurant}/>)}
+      
     </div>
     </div>
   )
